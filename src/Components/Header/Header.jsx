2982b62461efcc5c28c3dc864b995339d49e9ef5
sindhukasireddy <kasireddysindhureddy2003@gmail.com>
2024-05-20 18:15:54 +0530

    Initial Commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-scroll";
+import Bars from "../../assets/bars.png";
+
+const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
+  return (
+    <div className="header" id="header">
+      <div className="logo">FitTrack</div>
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt="bars"
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              activeClass="active"
+              to="header"
+              spy={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="programs"
+              spy={true}
+              smooth={true}
+            >
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="reasons"
+              spy={true}
+              smooth={true}
+            >
+              Why us
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="testimonials"
+              spy={true}
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="contact"
+              spy={true}
+              smooth={true}
+            >
+              Contact us
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="about"
+              spy={true}
+              smooth={true}
+            >
+              About us
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Header;
